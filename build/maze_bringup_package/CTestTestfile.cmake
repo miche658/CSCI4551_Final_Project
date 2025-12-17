fab0f20b522dc130c8c89d03b15cb9f0c3775dab
vu000194 <vu000194@umn.edu>
2025-12-16 19:14:03 -0600

    Readme added, bringup launches, git ignore added. Remove build, install, and log directories from version control

--- build/maze_bringup_package/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/miche658/CSCI4551/project/src/maze_bringup_package
-# Build directory: /home/miche658/CSCI4551/project/build/maze_bringup_package
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
